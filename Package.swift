ff4e436b470f89cd1da5f5a4a241823806885d3d
albert <albert@gmail.com>
2023-06-04 15:36:21 +0800

    feat：修改名称

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "AmplifyUILiveness",
+    name: "FCAmplifyUILiveness",
     defaultLocalization: "en",
     platforms: [.iOS(.v14)],
     products: [
